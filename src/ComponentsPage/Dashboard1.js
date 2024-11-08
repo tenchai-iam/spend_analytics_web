b6b2c8fe5312ff20b49136b9852d797bb23aba8e
Tenchai Iamthongthong <tenchai@iamthongtong.me>
2024-11-08 13:37:08 +0000

    Update 2 files
    - /src/ComponentsPage/TableD1Value.js
    - /src/ComponentsPage/Dashboard1.js

--- src/ComponentsPage/Dashboard1.js
+++ src/ComponentsPage/Dashboard1.js
@@ -68,7 +68,7 @@ const Dashboard1 = () => {
       matName: item.MAKTX,
       priceHQ: Number(item.PRICE_HQ),
       priceDistrict: Number(item.PRICE_REGION),
-      priceDiff: (Number(item.PRICE_DIFF) - 1) * 100,
+      priceDiff: (Number(item.PRICE_DIFF) - 1),
     })) || [];
 
   const {
@@ -85,11 +85,9 @@ const Dashboard1 = () => {
   const dataTableValue =
     top5POValue?.map((item) => ({
       district: item.DISTRICT_NAME,
-      percentQuantity: `${Number(item.PO_LESS_EQUAL_500K_QUANTITY).toFixed(
-        2
-      )}%`,
       lessThanQuantity: Number(item.PERCENT_PO_LESS),
       totalQuantity: Number(item.TOTAL_PO),
+      percentQuantity: Number(item.PO_LESS_EQUAL_500K_QUANTITY),
     })) || [];
 
   // Data mappings
