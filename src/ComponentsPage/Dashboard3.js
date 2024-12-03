77e128b06193802b07295237ce205da57d35b4d9
tenchai <tenchai@iamthongtong.me>
2024-12-03 20:11:36 +0700

    Update Dashboard3.js

--- src/ComponentsPage/Dashboard3.js
+++ src/ComponentsPage/Dashboard3.js
@@ -329,19 +329,12 @@ const Dashboard3 = () => {
                 value={selectedCategory}
                 onChange={(e) => setSelectedCategory(e.target.value)}
               >
-                <option value="">-- เลือกประเภทพัสดุ --</option>
-                {categoryData
-                  ?.slice() // Create a shallow copy of the array to avoid modifying the original
-                  .sort((a, b) => {
-                    if (a.CATEGORY_ID === "102") return -1; // Move `102` to the top
-                    if (b.CATEGORY_ID === "102") return 1;
-                    return a.CATEGORY_ID.localeCompare(b.CATEGORY_ID); // Default alphabetical sort by ID
-                  })
-                  .map((category, index) => (
-                    <option key={index} value={category.CATEGORY_ID}>
-                      {`${category.CATEGORY_ID}: ${category.CATEGORY_NAME}`}
-                    </option>
-                  ))}
+                <option value="">-- เลือกกลุ่มพัสดุ --</option>
+                {categoryData?.map((category, index) => (
+                  <option key={index} value={category.CATEGORY_ID}>
+                    {`${category.CATEGORY_ID}: ${category.CATEGORY_NAME}`}
+                  </option>
+                ))}
               </select>
             )}
           </div>
