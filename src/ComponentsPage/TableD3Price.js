bc6390aaea9b9168ddd32b194d4a7054442e46c4
tenchai <tenchai@iamthongtong.me>
2024-12-09 13:10:25 +0700

    Update TableD3Price.js

--- src/ComponentsPage/TableD3Price.js
+++ src/ComponentsPage/TableD3Price.js
@@ -15,7 +15,6 @@ const TableD3Price = ({ title, data }) => {
 
   const formatPercentage = (value) =>
     new Intl.NumberFormat("en-US", {
-      style: "percent",
       minimumFractionDigits: 2,
       maximumFractionDigits: 2,
     }).format(value);
