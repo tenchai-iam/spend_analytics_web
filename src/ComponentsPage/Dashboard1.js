541afa008130646417bad66a125cadbee2cb7ccc
tenchai <tenchai@iamthongtong.me>
2024-12-10 11:11:53 +0700

    Update Dashboard1.js

--- src/ComponentsPage/Dashboard1.js
+++ src/ComponentsPage/Dashboard1.js
@@ -51,7 +51,7 @@ const Dashboard1 = () => {
       style: "percent",
       minimumFractionDigits: 2,
       maximumFractionDigits: 2,
-    }).format(value / 100);
+    }).format(value);
 
   const formatTotal = (value) =>
     new Intl.NumberFormat("en-US", {
