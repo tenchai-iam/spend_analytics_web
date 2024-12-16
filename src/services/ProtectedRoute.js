de02a400edf271751f21622145d5091a3881e97b
tenchai <tenchai@iamthongtong.me>
2024-12-17 00:36:51 +0700

    Upload New File

---
+++ src/services/ProtectedRoute.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ element: Component }) => {
+  const token = sessionStorage.getItem("access_token");
+
+  // If no token, redirect to the login route
+  if (!token) {
+    window.location.href = "https://dev-spendi-tcc.pea.co.th/api/login";
+    return null;
+  }
+
+  return <Component />;
+};
+
+export default ProtectedRoute;
