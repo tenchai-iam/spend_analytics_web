f7d05e8ce01d33a856a97fc40cfb803bf1a8297d
tenchai <tenchai@iamthongtong.me>
2024-12-16 17:05:03 +0700

    Update keycloak.js

--- src/services/keycloak.js
+++ src/services/keycloak.js
@@ -1,27 +1,12 @@
 import Keycloak from "keycloak-js";
 
-// Initialize Keycloak with configuration
-const keycloak = new Keycloak({
-  url: "https://sso.pea.co.th/auth",
-  realm: "idm",
+// Initialize Keycloak with your configuration
+const keycloakConfig = {
+  url: "https://sso2.pea.co.th/realms/pea-users/protocol/openid-connect/auth",
+  realm: "pea-users",
   clientId: "pea-tcc-spend",
-});
-
-/**
- * Setup token refresh to keep the user logged in.
- */
-const setupTokenRefresh = () => {
-  setInterval(() => {
-    keycloak
-      .updateToken(30) // Refresh if token will expire in less than 30 seconds
-      .then((refreshed) => {
-        if (refreshed) console.log("Token refreshed:", keycloak.token);
-      })
-      .catch(() => {
-        console.error("Failed to refresh token. Logging out...");
-        keycloak.logout();
-      });
-  }, 60000); // Refresh every 60 seconds
 };
 
-export { keycloak, setupTokenRefresh };
+const keycloak = new Keycloak(keycloakConfig);
+
+export default keycloak;
