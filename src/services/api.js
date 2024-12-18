92edf110b0d1b865db284635ebce9353ee55a4d4
tenchai <tenchai@iamthongtong.me>
2024-12-18 18:35:40 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://spendi-tcc.pea.co.th/api";
+const API_URL = "https://dev-spendi-tcc.pea.co.th/api";
 
 // Fetch years from the API
 export const getYears = async () => {
