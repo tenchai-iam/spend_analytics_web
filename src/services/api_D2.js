e246be73fa2884803a5a6916ed9a4f623fcfa442
tenchai <tenchai@iamthongtong.me>
2024-12-19 13:37:29 +0700

    Update api_D2.js

--- src/services/api_D2.js
+++ src/services/api_D2.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://dev-spendi-tcc.pea.co.th/api";
+const API_URL = process.env.REACT_APP_API_URL;
 
 // Fetch top supplier data for a specific year and category using a POST request
 export const getD2TopSupplier = async (year, category_group) => {
