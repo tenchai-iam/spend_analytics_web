a1d1eaae320c8fafe7c6eeea42aeefc276ecd87e
tenchai-iam <tenchai@iamthongtong.me>
2025-01-07 11:29:39 +0000

    Remove protected routes

--- src/App.js
+++ src/App.js
@@ -25,12 +25,27 @@ export default function App() {
       <Router>
         <div className="App">
           <Routes>
-            <Route path="/" element={<ProtectedRoute element={Home} />} />
-            <Route path="/dashboard1" element={<ProtectedRoute element={Dashboard1} />} />
-            <Route path="/dashboard2" element={<ProtectedRoute element={Dashboard2} />} />
-            <Route path="/dashboard3" element={<ProtectedRoute element={Dashboard3} />} />
-            <Route path="/dashboard4" element={<ProtectedRoute element={Dashboard4} />} />
-            <Route path="/upload" element={<ProtectedRoute element={Upload} />} />
+            <Route path="/" element={Home} />
+            <Route
+              path="/dashboard1"
+              element={<ProtectedRoute element={Dashboard1} />}
+            />
+            <Route
+              path="/dashboard2"
+              element={<ProtectedRoute element={Dashboard2} />}
+            />
+            <Route
+              path="/dashboard3"
+              element={<ProtectedRoute element={Dashboard3} />}
+            />
+            <Route
+              path="/dashboard4"
+              element={<ProtectedRoute element={Dashboard4} />}
+            />
+            <Route
+              path="/upload"
+              element={<ProtectedRoute element={Upload} />}
+            />
             <Route path="/callback" element={<Callback />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
