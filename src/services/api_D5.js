c798df764d595e34baa7c68f8887938dde3a5db4
tenchai-iam <tenchai@iamthongtong.me>
2025-02-14 00:45:30 +0000

    Dash 5 API

--- src/services/api_D5.js
+++ src/services/api_D5.js
@@ -5,7 +5,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 // Fetch baseline, normalized and actual value for planned from the API
 export const getPlannedValue = async (year) => {
   const response = await axios.post(
-    `${API_URL}/planned_stage5_value`,
+    `${API_URL}/planned_PI_stage5_value`,
     {
       year: year, // Pass the year value in the request body
     },
@@ -16,12 +16,12 @@ export const getPlannedValue = async (year) => {
 
 // Fetch baseline, normalized and actual value for planned from the API
 export const getUnplannedValue = async (year) => {
-    const response = await axios.post(
-      `${API_URL}/unplanned_stage5_value`,
-      {
-        year: year, // Pass the year value in the request body
-      },
-      { timeout: 5000 }
-    );
-    return response.data;
-  };
+  const response = await axios.post(
+    `${API_URL}/unplanned_PI_stage5_value`,
+    {
+      year: year, // Pass the year value in the request body
+    },
+    { timeout: 5000 }
+  );
+  return response.data;
+};
