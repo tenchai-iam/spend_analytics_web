d5e32c61fbd3c8d80ba141c103fe7edb15920287
tenchai-iam <tenchai@iamthongtong.me>
2025-05-18 03:48:18 +0000

    Update Dash3

--- src/services/api_D3.js
+++ src/services/api_D3.js
@@ -35,6 +35,18 @@ export const getD3CategoryPriceTable = async (year, category_id) => {
 };
 
 // Fetch average spend data for a specific year and category using a POST request
+export const getD3CategoryPriceTable12M = async (category_id) => {
+  const response = await axios.post(
+    `${API_URL}/dashboard_3_get_price_group_12m`,
+    {
+      category_id: String(category_id), // Pass the category ID value in the request body
+    },
+    { timeout: 5000 }
+  );
+  return response.data;
+};
+
+// Fetch average spend data for a specific year and category using a POST request
 export const getD3MaterialPriceGroupDistrict = async (year, matnr) => {
   const response = await axios.post(
     `${API_URL}/dashboard_3_get_price_details`,
